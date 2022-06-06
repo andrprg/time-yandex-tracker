55ff6009df5182066ced598c049c0aa6de85750d
andrprg <andrprg@rambler.ru>
2022-06-06 15:58:06 +0300

    feat(interceptors): добавлены интерсепторы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiPrefixInterceptor } from './data/interceptors/api-prefix.interceptor';
+import { HeaderInterceptor } from './data/interceptors/header.interceptor';
+import { TestComponent } from './presantation/pages/test/test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, TestComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiPrefixInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HeaderInterceptor,
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
